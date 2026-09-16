9a0a2728e5716f2cac54a76a3dc2affe56f5f7cb
ektihaile <ektihaile12@gmail.com>
2026-09-16 07:29:57 -1200

    Implement: Resource-Based Authorization

--- Tms-client/src/app/app.config.ts
+++ Tms-client/src/app/app.config.ts
@@ -1,7 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideHttpClient, withInterceptors, withXsrfConfiguration } from '@angular/common/http';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { credentialsInterceptor } from './interceptors/credentials.interceptor';
 import { errorInterceptor } from './interceptors/error.interceptor';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,6 +13,7 @@ export const appConfig: ApplicationConfig = {
         cookieName: 'XSRF-TOKEN',
         headerName: 'X-XSRF-TOKEN',
       })
-    )
+    ),
+    provideRouter(routes, withComponentInputBinding()),
   ]
 };
